273947fa1b55a22bf9927dc10e2845dd7cd9a8a3
Ashley Baldry <arbaldry91@gmail.com>
2022-08-14 16:03:41 +0100

    Add more tests

--- tests/testthat/test-hp.R
+++ tests/testthat/test-hp.R
@@ -1,8 +1,12 @@
 test_that("Hit Point calculations are correct", {
   # Using results from https://bulbapedia.bulbagarden.net/wiki/Stat
-  hp <- calculate_hp_v1(35, 81, 7, 22850)
+  hp <- calculate_hp_v1(base_hp = 35, level = 81, dv = 7, ev = 22850)
+  expect_identical(hp, 189L)
+  hp <- calculate_hp(base_hp = 35, level = 81, iv = 7, ev = 22850, generation = 1)
   expect_identical(hp, 189L)
 
-  hp <- calculate_hp_v2(108, 78, 24, 74)
+  hp <- calculate_hp_v2(base_hp = 108, level = 78, iv = 24, ev = 74)
+  expect_identical(hp, 289L)
+  hp <- calculate_hp(base_hp = 108, level = 78, iv = 24, ev = 74)
   expect_identical(hp, 289L)
 })
