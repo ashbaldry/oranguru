267cd80d50bd00e9b0035deba90699549838a39e
Ashley Baldry <arbaldry91@gmail.com>
2023-02-05 19:11:55 +0000

    Fix failing Pokemon class tests

--- tests/testthat/test-pokemon.R
+++ tests/testthat/test-pokemon.R
@@ -1,11 +1,17 @@
 test_that("New Pokemon can be generated using PokeAPI data", {
   bulbasaur_data <- readRDS("data/bulbasaur.rds")
-  bulbasaur <- Pokemon$new(bulbasaur_data)
+  bulbasaur <- Pokemon$new(api_data = bulbasaur_data, generation = 1L)
 
   expect_identical(class(bulbasaur), c("pokemon", "R6"))
-  expect_identical(bulbasaur$.__enclos_env__$private$name, "Bulbasaur")
+  expect_identical(bulbasaur$get_stat("name"), "Bulbasaur")
+  expect_length(bulbasaur$get_moves(), 4L)
 
-  bulbasaur2 <- Pokemon$new(bulbasaur_data, generation = 1L)
+  crit_chance <- bulbasaur$get_crit_chance(bulbasaur$get_stat("move_1"))
+  expect_type(crit_chance, "double")
+  expect_gte(crit_chance, 0L)
+  expect_lte(crit_chance, 1L)
+
+  bulbasaur2 <- Pokemon$new(api_data = bulbasaur_data, generation = 8L)
   expect_identical(class(bulbasaur2), c("pokemon", "R6"))
-  expect_identical(bulbasaur2$.__enclos_env__$private$name, "Bulbasaur")
+  expect_identical(bulbasaur2$get_stat("name"), "Bulbasaur")
 })
